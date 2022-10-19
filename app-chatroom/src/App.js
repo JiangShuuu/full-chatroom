0151082f358ad0b5273c20d27ed4ea618ad6f44e
JiangShuuu <globelex65@gmail.com>
2022-10-19 22:38:58 +0800

    add style register

--- app-chatroom/src/App.js
+++ app-chatroom/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Chat from './pages/Chat'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/" element={<Chat/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
