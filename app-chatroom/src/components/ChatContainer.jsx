38fb4dcaca9ded20b4d4ffeaaf17da789903ed43
JiangShuuu <globelex65@gmail.com>
2022-10-27 21:57:26 +0800

    upd

--- app-chatroom/src/components/ChatContainer.jsx
+++ app-chatroom/src/components/ChatContainer.jsx
@@ -65,7 +65,7 @@ export default function ChatContainer({ currentChat, currentUser, socket }) {
         setArrivalMessage({ fromSelf: false, message: msg });
       });
     }
-  }, []);
+  }, [socket]);
 
   useEffect(() => {
     arrivalMessage && setMessages((prev) => [...prev, arrivalMessage]);
